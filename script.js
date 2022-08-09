f0e40e3f32de3341eba5e480ab05cdf1b9889b2c
Hilario Torres <hilatorres2002@gmail.com>
2022-08-08 23:21:09 -0300

    segundaEntregaDoms

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/*
 class Juego {
 
     constructor(Nombre, Desarrollador, Logros, Precio, FechaSalida) {
@@ -15,137 +14,116 @@ class Juego {
 
 const juego1 = new Juego("Counter-Strike", "Valve", 167, 129.99, 2000);
 
-const juego2 = new Juego("Valorant", "Riot Games", 0, "Gratis", 2020);
+const juego2 = new Juego("Valorant", "Riot", 0, "Gratis", 2020);
 
-const juego3 = new Juego("Grand thef auto V", "Rockstar Games", 77, 850, 2013);
+const juego3 = new Juego("GTA 5", "Rockstar", 77, 850, 2013);
 
 const juego4 = new Juego("Gang Beasts", "Boneloaf", 19, 224.99, 2017);
 
-const juego5 = new Juego("Cuphead", "Studio MDHR", 42, 224.99, 2017);
+const juego5 = new Juego("Cuphead", "MDHR", 42, 224.99, 2017);
 
 const juegos = [juego1, juego2, juego3, juego4, juego5];
 
+const botonIniciar = document.getElementById("boton1");
 
+const sectionIniciar1 = document.getElementById("textoSection1");
 
-let bienvenida;
+const sectionIniciar2 = document.getElementById("textoSection2");
 
+const sectionIniciar3 = document.getElementById("textoSection3");
 
-function iniciar() {
+const sectionIniciar4 = document.getElementById("textoSection4");
 
-    do {
-        bienvenida = parseFloat(prompt("Le damos la bienvenida a nuestra tienda de juegos, seleccione una opción\n\n 1- Ver nuestro catalogo \n 2-Ver Nuestros Precios \n 3-Salir"));
-    } while (isNaN(bienvenida));
+const sectionIniciar5 = document.getElementById("textoSection5");
 
+function Comenzar5() {
+juegos.forEach(Juego => {
+    sectionIniciar1.innerHTML = `
+    <div class="card shadow e">
+<img src="./pictures/1.JPG" alt="logo1">
+        <p>Nombre: ${juego5.Nombre}</p>
+        <p>Desarrollador: ${juego5.Desarrollador}</p>
+        <p>Logros: ${juego5.Logros}</p>
+        <p>Precio: ${juego5.Precio}</p>
+        <p>Fecha de salida: ${juego5.FechaSalida}</p>
+    </div>
+`
+    })
 }
 
-iniciar();
+function Comenzar1() {
+    juegos.forEach(Juego => {
+        sectionIniciar2.innerHTML = `
+        <div class="card shadow e">
+    <img src="./pictures/4.jpg" alt="logo1">
+            <p>Nombre: ${juego1.Nombre}</p>
+            <p>Desarrollador: ${juego1.Desarrollador}</p>
+            <p>Logros: ${juego1.Logros}</p>
+            <p>Precio: ${juego1.Precio}</p>
+            <p>Fecha de salida: ${juego1.FechaSalida}</p>
+        </div>
+    `
+        })
+    }
 
-const mostrarJuegos = juegos.map(juegosArray => juegosArray.Nombre);
-const mostrarPrecios = juegos.map(juegosArray => juegosArray.Precio);
-let preguntavolver;
-let preguntaImpuesto;
+function Comenzar2() {
+    juegos.forEach(Juego => {
+        sectionIniciar3.innerHTML = `
+        <div class="card shadow e">
+    <img src="./pictures/5.jpg" alt="logo1">
+            <p>Nombre: ${juego2.Nombre}</p>
+            <p>Desarrollador: ${juego2.Desarrollador}</p>
+            <p>Logros: ${juego2.Logros}</p>
+            <p>Precio: ${juego2.Precio}</p>
+            <p>Fecha de salida: ${juego2.FechaSalida}</p>
+        </div>
+    `
+        })
+    }
 
-if (bienvenida === 1) {
-    console.log(`Nuestros Juegos son ${mostrarJuegos}`);
-    do {
-        preguntavolver = parseFloat(prompt("¿Quiere volver al menú anterior?\n\n 1- ¿Quiere ver mas datos de nuestros juegos? \n 2-Deseo irme"))
-    } while (isNaN(preguntavolver));
-
-}
-if (bienvenida === 2) {
-    console.log(`Nuestros precios son ${mostrarPrecios}`);
-    do {
-        preguntaImpuesto = parseFloat(prompt("¿Quiere previsualizar cuál es el precio final?\n\n 1- Si \n 2-Deseo irme"))
-    } while (isNaN(preguntaImpuesto));
-}
-if (bienvenida === 3) {
-    alert("Muchas Gracias por usar nuestro servicio, vuelva cuando quiera \n\n :D");
-}
-if (bienvenida > 3) {
-    console.log("Ingrese un valor valido");
-}
-
-const lanzamiento = juegos.map(juegosArray => juegosArray.FechaSalida);
-const desarroladorJuego = juegos.map(juegosArray => juegosArray.Desarrollador);
-const logrosJuego = juegos.map(juegosArray => juegosArray.Logros);
-const datosJuegos = [mostrarJuegos, desarroladorJuego, lanzamiento, logrosJuego];
-
-if (preguntavolver === 1) {
-    console.table(datosJuegos)
-
-}
-if (preguntavolver === 2) {
-    alert("Muchas Gracias por usar nuestro servicio, vuelva cuando quiera \n\n :D");
-}
-if (preguntavolver > 2)
-// No sabia como solucionar una cosa, cuando ponia un numero que no era 1 o 2 se iba el prompt y si ponia un else no me lo tomaba, me saltaba el console.log("Ingrese un valor valido") en cualquier cosa diferente a 1 o 2 (la solucion que enocontre fue un mayor que dos pero me gustaria encontrar otra)
-{
-    console.log("Ingrese un valor valido");
-}
-
-let ingresePrecio;
-
-function realizarImpuesto() {
-
-    for (let i = ingresePrecio; i = i * 75 / 100 + i;) {
-        alert(`El resulado con impuesto es ${i} \n Gracias por utilizarnos`)
-        if (i != ingresePrecio) {
-            break
+    function Comenzar3() {
+        juegos.forEach(Juego => {
+            sectionIniciar4.innerHTML = `
+            <div class="card shadow e">
+        <img src="./pictures/2.JPG" alt="logo1">
+                <p>Nombre: ${juego3.Nombre}</p>
+                <p>Desarrollador: ${juego3.Desarrollador}</p>
+                <p>Logros: ${juego3.Logros}</p>
+                <p>Precio: ${juego3.Precio}</p>
+                <p>Fecha de salida: ${juego3.FechaSalida}</p>
+            </div>
+        `
+            })
         }
-    }
-}
 
-let seleccioneJuego;
-
-
-
-if (preguntaImpuesto === 1) {
-
-    do {
-        seleccioneJuego = parseFloat(prompt("¿Que juego desea aplicarle el impuesto?\n\n 1-Counter-Strike\n 2-Valorant\n 3-Grand thef auto V\n 4-Gang Beasts\n 5-Cuphead"))
-    } while (isNaN(seleccioneJuego));
-    if (seleccioneJuego === 1) {
-        ingresePrecio = 129.99 //Trate de poner el precio mediante el objeto, pero la verdad que no pude (Osea que se mantenga actualizado si cambio el precio en el objeto)
-        realizarImpuesto()
-    }
-    if (seleccioneJuego === 2) {
-        alert("El juego es gratis")
-    }
-    if (seleccioneJuego === 3) {
-        ingresePrecio = 850
-        realizarImpuesto()
-    }
-    if (seleccioneJuego === 4) {
-        ingresePrecio = 224.99
-        realizarImpuesto()
-    }
-    if (seleccioneJuego === 5) {
-        ingresePrecio = 224.99
-        realizarImpuesto()
-    }
-    if (seleccioneJuego > 5) {
-        console.log("Ingrese un valor valido");
-    }
-
-}
-if (preguntaImpuesto === 2) {
-    alert("Muchas Gracias por usar nuestro servicio, vuelva cuando quiera \n\n :D");
-}
-if (preguntaImpuesto > 2) {
-    console.log("Ingrese un valor valido");
-}
+        function Comenzar4() {
+            juegos.forEach(Juego => {
+                sectionIniciar5.innerHTML = `
+                <div class="card shadow e">
+            <img src="./pictures/3.jpg" alt="logo1">
+                    <p>Nombre: ${juego4.Nombre}</p>
+                    <p>Desarrollador: ${juego4.Desarrollador}</p>
+                    <p>Logros: ${juego4.Logros}</p>
+                    <p>Precio: ${juego4.Precio}</p>
+                    <p>Fecha de salida: ${juego4.FechaSalida}</p>
+                </div>
+            `
+                })
+            }
 
 
- */ 
-
-const div1 = document.getElementById("div1");
-
-const div2 = document.getElementById("div2");
-
-const div3 = document.getElementById("div3");
-
-div1.innerText = "Sean bienvenidos a nuestra maravillosa tienda de juegos :D";
-
-div2.innerText = "Tenemos el mejor catalogo :)";
-
-div3.innerText = "Con las mejores ofertas y los impuestos mas abusivos :o";
+    botonIniciar.addEventListener("click",()=>{
+        Comenzar1();
+    });
+    botonIniciar.addEventListener("click",()=>{
+        Comenzar2();
+    });
+    botonIniciar.addEventListener("click",()=>{
+        Comenzar3();
+    });
+    botonIniciar.addEventListener("click",()=>{
+        Comenzar4();
+    });
+    botonIniciar.addEventListener("click",()=>{
+        Comenzar5();
+    });
